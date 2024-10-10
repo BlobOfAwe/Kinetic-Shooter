19f9c9f52664af07d82b51dc51ba98a48a16dcb8
BigN656 <nathanielklassen99@gmail.com>
2024-10-10 15:09:45 -0700

    No Duplicate Generation
    Implemented an alternative generation method that guarantees no duplicate chunks and added a check so that it can’t lock up the program with an infinite loop if there are fewer chunks than spaces on the grid.

--- Assets/MapRandomization/Scripts/MapGenerator.cs
+++ Assets/MapRandomization/Scripts/MapGenerator.cs
@@ -10,6 +10,8 @@ public class MapGenerator : MonoBehaviour
 
     private Transform[,] positions;
 
+    private int[,] randomValues;
+
     [System.Serializable]
     private struct TransformStruct
     {
@@ -19,6 +21,7 @@ public class MapGenerator : MonoBehaviour
     private void Awake()
     {
         positions = new Transform[positionRows[0].transforms.Length, positionRows.Length];
+        randomValues = new int[positions.GetLength(0), positions.GetLength(1)];
         for (int y = 0; y < positionRows.Length; y++)
         {
             for (int x = 0; x < positionRows[y].transforms.Length; x++)
@@ -35,7 +38,47 @@ public class MapGenerator : MonoBehaviour
             for (int y = 0; y < positions.GetLength(1); y++)
             {
                 int r = Random.Range(0, chunks.Length);
-                Debug.Log("r[" + x + ", " + y + "] = " + r);
+                Instantiate(chunks[r], positions[x, y]);
+            }
+        }
+    }
+
+    public void GenerateNoDuplicate()
+    {
+        for (int x = 0; x < randomValues.GetLength(0); x++)
+        {
+            for (int y = 0; y < randomValues.GetLength(1); y++)
+            {
+                randomValues[x, y] = -1;
+            }
+        }
+
+        for (int x = 0; x < positions.GetLength(0); x++)
+        {
+            for (int y = 0; y < positions.GetLength(1); y++)
+            {
+                int r = Random.Range(0, chunks.Length);
+                for (int rx = 0; rx < randomValues.GetLength(0); rx++)
+                {
+                    if (chunks.Length < positions.GetLength(0) * positions.GetLength(1))
+                    {
+                        Debug.LogWarning("Preventing duplicate chunks is not supported when there are fewer chunks than there are squares in the grid.");
+                        break;
+                    }
+                    for (int ry = 0; ry < randomValues.GetLength(1); ry++)
+                    {
+                        while (r == randomValues[rx, ry])
+                        {
+                            r = Random.Range(0, chunks.Length);
+                            if (r != randomValues[rx, ry])
+                            {
+                                rx = 0;
+                                ry = 0;
+                            }
+                        }
+                    }
+                }
+                randomValues[x, y] = r;
                 Instantiate(chunks[r], positions[x, y]);
             }
         }
