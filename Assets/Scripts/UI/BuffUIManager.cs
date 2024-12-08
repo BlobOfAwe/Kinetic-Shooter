02ee348c219e5f23d6959bf5572a73e3001337e3
ZeeTheBeee <s.sheikhanzai@gmail.com>
2024-12-08 03:04:17 -0800

    BuffsImplementation
    Created 3 scripts to handle the buff display, system works for now but there isn't a differntiation between buffs and debuffs as of this moment, will work on it later.

---
+++ Assets/Scripts/UI/BuffUIManager.cs
@@ -0,0 +1,26 @@
+//Z.S
+//This script handles the creation and removal of the buffs using buffdata.
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BuffUIManager : MonoBehaviour
+{
+    public GameObject buffUIPrefab;
+    public Transform buffUIContainer;
+    private Dictionary<Buff, GameObject> activeBuffs = new Dictionary<Buff, GameObject>();
+
+    public void AddBuff(Buff buff)
+    {
+            GameObject buffUI = Instantiate(buffUIPrefab, buffUIContainer);
+            buffUI.GetComponent<BuffUI>().Initialize(buff);
+            activeBuffs.Add(buff, buffUI);
+    }
+    public void RemoveBuff(Buff buff)
+    {
+        if (activeBuffs.ContainsKey(buff))
+        {
+            Destroy(activeBuffs[buff]);
+            activeBuffs.Remove(buff);
+        }
+    }
+}
