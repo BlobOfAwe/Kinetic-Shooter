2133396d1c60be955e4a6317c9bfed3b0527f039
BlobOfAwe <jamescvm03@gmail.com>
2024-12-11 12:34:23 -0800

    Balance updates

--- Assets/Scripts/Items/GenericBuffDebuff.cs
+++ Assets/Scripts/Items/GenericBuffDebuff.cs
@@ -58,15 +58,7 @@ public class GenericBuffDebuff : Item
         buffConstructor.duration = duration;
         buffTarget.ApplyBuff(buffConstructor);
         FindObjectOfType<BuffUIManager>().AddBuff(buffConstructor);//Added by Z.S
-        // Apply the buff to the entity
-        StartCoroutine(ActiveBuff(duration));
+
     }
 
-    IEnumerator ActiveBuff(float duration)
-    {
-        yield return new WaitForSeconds(duration);
-        transform.parent = null;
-        buffTarget = null;
-        gameObject.SetActive(false);
-    }
 }
