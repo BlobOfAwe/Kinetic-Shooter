4ed3edc93f1ea5e93a38ebcddf9864342f80e3b9
BigN656 <nathanielklassen99@gmail.com>
2024-12-10 23:08:55 -0800

    Score Manager
    Created a system that adds points to the player's score when defeating enemies and persists between scenes.

---
+++ Assets/WinConditionBranch/ScoreManager.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ScoreManager : MonoBehaviour
+{
+    [SerializeField]
+    private int score = 0;
+
+    private static ScoreManager instance;
+
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+        if (instance == null)
+        {
+            instance = this;
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        } else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+    {
+        Debug.Log("Scene Loaded");
+        if (FindObjectOfType<ScoreDisplay>() != null)
+        {
+            FindObjectOfType<ScoreDisplay>().UpdateScore(score);
+            Debug.Log("Score updated to " + score + " for scene: " + scene.name);
+        }
+    }
+
+    public void AddPoints(int points)
+    {
+        score += points;
+        Debug.Log("+" + points + " points!");
+        if (FindObjectOfType<ScoreDisplay>() != null)
+        {
+            FindObjectOfType<ScoreDisplay>().UpdateScore(score);
+        } else
+        {
+            Debug.Log("Score: " + score);
+        }
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+        if (FindObjectOfType<ScoreDisplay>() != null)
+        {
+            FindObjectOfType<ScoreDisplay>().UpdateScore(score);
+        }
+    }
+}
