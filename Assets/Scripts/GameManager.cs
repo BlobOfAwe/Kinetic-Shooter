03627945391c2ba1cf5fdc3c5a219c4c9812602b
BlobOfAwe <jamescvm03@gmail.com>
2025-01-17 13:15:32 -0800

    Balance Tweaks
    Updated enemy stats for better balance
    Added a Difficulty Coefficient to increase difficulty across levels.
    Fixed HP bar to accurately reflect current HP

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,6 +5,17 @@ using UnityEngine;
 
 public static class GameManager
 {
+    /// <summary>
+    /// The Difficulty Coefficient tracks the game's difficulty across levels.
+    /// References to the coefficient should look as follows:
+    ///     _value = _baseValue * ( 1 + (difficultyCoefficient * _modifier))
+    /// As an example:
+    ///     attackStat = 10 * ( 1 + (difficultyCoefficient * 0.1))
+    ///     This increases the attack stat by 10% of its base value for every increment of 1 of the coefficient
+    /// The difficulty Coefficient increases by 1 every level.
+    /// </summary>
+    public static float difficultyCoefficient = 0;
+
     public static bool debugEnabled;
     public static LoadoutManager.Loadout playerLoadout;
     public static bool paused;
