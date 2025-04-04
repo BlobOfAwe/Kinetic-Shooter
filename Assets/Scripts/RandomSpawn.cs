a565a6281cec9d913afd757f4b73d93456a7d5ab
BigN656 <nathanielklassen99@gmail.com>
2025-04-04 01:16:39 -0700

    Spawn Area Fix
    Fixed an issue where the player and beacon wouldn't spawn near the center of the map anymore.

--- Assets/Scripts/RandomSpawn.cs
+++ Assets/Scripts/RandomSpawn.cs
@@ -4,6 +4,21 @@ using UnityEngine;
 
 public class RandomSpawn : MonoBehaviour
 {
+    [SerializeField]
+    private bool wholeMap = true;
+
+    [SerializeField]
+    private float xMin = 0f;
+
+    [SerializeField]
+    private float xMax = 0f;
+
+    [SerializeField]
+    private float yMin = 0f;
+
+    [SerializeField]
+    private float yMax = 0f;
+
     private MapGenerator mapGenerator;
 
     private Vector2 spawnPosition;
@@ -15,13 +30,15 @@ public class RandomSpawn : MonoBehaviour
 
     public void Spawn()
     {
-        if (mapGenerator != null)
+        if (wholeMap && mapGenerator != null)
         {
             spawnPosition.x = Random.Range(mapGenerator.levelXMin, mapGenerator.levelXMax);
             spawnPosition.y = Random.Range(mapGenerator.levelYMin, mapGenerator.levelYMax);
-        } else
+        }
+        else
         {
-            spawnPosition = Vector2.zero;
+            spawnPosition.x = Random.Range(xMin, xMax);
+            spawnPosition.y = Random.Range(yMin, yMax);
         }
         transform.position = spawnPosition;
         GraphNode node = AstarPath.active.GetNearest(transform.position, NNConstraint.Default).node;
