65a1e4646c0ecf1f7b7fca5a5e6c6662a2c2d12f
BigN656 <nathanielklassen99@gmail.com>
2025-04-04 13:48:24 -0700

    Bug Fixes
    Various bug fixes and improvements, including fixing the tutorial level and the train.

--- Assets/Scripts/Environment/TutorialBeacon.cs
+++ Assets/Scripts/Environment/TutorialBeacon.cs
@@ -7,11 +7,12 @@ using UnityEngine.Rendering;
 
 public class TutorialBeacon : Beacon
 {
-
+    [SerializeField]
+    private int menuScene = 0;
 
     protected override IEnumerator WinGame()
     {
         yield return new WaitForSeconds(1.5f);
-        SceneManager.LoadSceneAsync(0);
+        SceneManager.LoadSceneAsync(menuScene);
     }
 }
