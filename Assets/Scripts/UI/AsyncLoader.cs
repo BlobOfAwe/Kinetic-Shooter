e52b3eff021323cf895e9ac7ddaa6ee33a04aa0b
BlobOfAwe <jamescvm03@gmail.com>
2025-04-12 07:50:21 -0700

    Fix scene loading

--- Assets/Scripts/UI/AsyncLoader.cs
+++ Assets/Scripts/UI/AsyncLoader.cs
@@ -11,7 +11,7 @@ public class AsyncLoader : MonoBehaviour
     {
         DataManager.Instance.SaveGame(); // Automatically saves the game when a scene changes. - NK
         // If you are loading from the main menu scene, the run is starting, set the difficulty to 0
-        if (SceneManager.GetActiveScene().buildIndex == 0)
+        if (SceneManager.GetActiveScene().buildIndex == 1)
         {
             GameManager.difficultyCoefficient = 0;
             GameManager.currentLevel = 0;
