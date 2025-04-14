a108029da2535cbf3d8854fcb0c1bef409d8bb98
BigN656 <nathanielklassen99@gmail.com>
2025-04-14 14:30:19 -0700

    Cooldown Prompts
    Improved cooldown display and added button prompts that dynamically change between keyboard and gamepad prompts depending on the last input.

---
+++ Assets/NK-ShowcaseImprovements/Scripts/ButtonPrompt.cs
@@ -0,0 +1,55 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.UI;
+
+public class ButtonPrompt : MonoBehaviour
+{
+    [SerializeField]
+    private bool isUI = true;
+    [SerializeField]
+    private Sprite keyboardSprite;
+    [SerializeField]
+    private Sprite gamepadSprite;
+
+    private Image image;
+    private SpriteRenderer spriteRenderer;
+    private PlayerInput playerInput;
+
+    private void Awake()
+    {
+        if (isUI)
+        {
+            image = GetComponent<Image>();
+        } else
+        {
+            spriteRenderer = GetComponent<SpriteRenderer>();
+        }
+        playerInput = FindObjectOfType<PlayerInput>();
+    }
+
+    private void Update()
+    {
+        if (playerInput != null)
+        {
+            if (playerInput.currentControlScheme == "Keyboard+Mouse")
+            {
+                if (isUI)
+                {
+                    image.sprite = keyboardSprite;
+                } else
+                {
+                    spriteRenderer.sprite = gamepadSprite;
+                }
+            } else if (playerInput.currentControlScheme == "Gamepad")
+            {
+                if (isUI)
+                {
+                    image.sprite = gamepadSprite;
+                } else
+                {
+                    spriteRenderer.sprite = gamepadSprite;
+                }
+            }
+        }
+    }
+}
--- Assets/Scripts/UI/Cooldown.cs
+++ Assets/Scripts/UI/Cooldown.cs
@@ -9,7 +9,21 @@ public class Cooldown : MonoBehaviour
     public Image primaryCooldown;
     public Image secondaryCooldown;
     public Image utilityCooldown;
-    public Image additionalCooldown;
+    //public Image additionalCooldown; - Removed by Nathaniel Klassen
+    // Following variables and all to do with them added by Nathaniel Klassen
+    public Image primaryButtonPrompt;
+    public Image secondaryButtonPrompt;
+    public Image utilityButtonPrompt;
+
+    // Added by Nathaniel Klassen
+    [SerializeField]
+    private Color cooldownFullColor = Color.white;
+    [SerializeField]
+    private Color cooldownWaitColor = Color.white;
+    [SerializeField]
+    private Color buttonFullColor = Color.white;
+    [SerializeField]
+    private Color buttonWaitColor = Color.white;
 
     private void Start()
     {
@@ -18,22 +32,28 @@ public class Cooldown : MonoBehaviour
 
     private void Update()
     {
-        UpdateCooldownDisplay(primaryCooldown, player.primary);
-        UpdateCooldownDisplay(secondaryCooldown, player.secondary);
-        UpdateCooldownDisplay(utilityCooldown, player.utility);
-        UpdateCooldownDisplay(additionalCooldown, player.additional);
+        UpdateCooldownDisplay(primaryCooldown, primaryButtonPrompt, player.primary);
+        UpdateCooldownDisplay(secondaryCooldown, secondaryButtonPrompt, player.secondary);
+        UpdateCooldownDisplay(utilityCooldown, utilityButtonPrompt, player.utility);
+        //UpdateCooldownDisplay(additionalCooldown, player.additional); - Removed by Nathaniel Klassen
     }
 
-    private void UpdateCooldownDisplay(Image cooldownImage, Ability ability)
+    private void UpdateCooldownDisplay(Image cooldownImage, Image buttonPrompt, Ability ability)
     {
         if (ability == null || cooldownImage == null) return;
 
         float ratio = Mathf.Clamp01(ability.currentCooldown / ability.cooldown);
         float fillAmount = 1 - Mathf.InverseLerp(0, ability.cooldown, ability.currentCooldown);
 
+        // Changed to use color variables set in inspector instead of hard-coded colors. - NK
         cooldownImage.fillAmount = fillAmount;
         cooldownImage.color = fillAmount < 1 ?
-            new Color(0.8f, 0.8f, 0.8f, 0.5f) :
-            new Color(1, 1, 1, 0.2f);
+            cooldownWaitColor :
+            cooldownFullColor;
+
+        buttonPrompt.fillAmount = fillAmount;
+        buttonPrompt.color = fillAmount < 1 ?
+            buttonWaitColor :
+            buttonFullColor;
     }
 }
